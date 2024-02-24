37f0f2b620b4855ea1d976753beea27165984ba7
alfiensukma <alfien.sukmaprawira1724@gmail.com>
2024-02-24 11:39:21 +0700

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 211, 213, 244);
+const kShrinePink100 = Color.fromARGB(255, 184, 188, 238);
+const kShrinePink300 = Color.fromARGB(255, 145, 135, 215);
+const kShrinePink400 = Color.fromARGB(255, 92, 83, 196);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
+
+const kShrinePurple = Color(0xFF5D1049);
